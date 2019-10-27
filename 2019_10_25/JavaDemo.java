b4235332485c13af85f0c3c2a75b6f699c2943b5
Codemater_Han <13629279398@163.com>
2019-10-27 08:24:23 +0800

    提交02

---
+++ 2019_10_25/JavaDemo.java
@@ -0,0 +1,17 @@
+public class JavaDemo
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		System.out.println(a);
+		byte minByte=Byte.MIN_VALUE;
+		byte maxByte=Byte.MAX_VALUE;
+		System.out.println(minByte);
+		System.out.println(maxByte);
+		
+		System.out.println("===========================");
+		Long bInt=20L;
+		System.out.println(bInt);
+		
+	}
+}
